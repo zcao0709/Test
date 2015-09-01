d75e15cc8ff89674a43b9cd197895fadb401f925
zcao0709 <zcao0709@gmail.com>
2015-09-01 12:50:59 +0800

    BinarySearchTree AVLTree

---
+++ src/com/alex/test/AVLTree.java
@@ -0,0 +1,329 @@
+package com.alex.test;
+
+import java.util.Scanner;
+
+public class AVLTree {
+
+	private Node root;
+
+	public AVLTree(int[] data) {
+		root = new Node(data[0]);
+		makeTree(root, data, 0);
+	}
+	
+	public AVLTree() {
+		root = null;
+	}
+
+	public void printTree() {
+		inorder(root);
+		preorder(root);
+	}
+
+	private void inorder(Node node) {
+		if (node == null)
+			return;
+		inorder(node.left);
+		System.out.println(node.val + ", " + node.ht);
+		inorder(node.right);
+	}
+	
+	private void preorder(Node node) {
+		if (node == null)
+			return;
+		System.out.println(node.val);
+		preorder(node.left);
+		preorder(node.right);
+	}
+
+	public boolean isValid() {
+		return isValid(root);
+	}
+
+	public int sum() {
+		return sum(root);
+	}
+
+	private int sum(Node node) {
+		if (node == null)
+			return 0;
+		return node.val + sum(node.left) + sum(node.right);
+	}
+
+	private boolean isLeft(Node node) {
+		return node == node.parent.left;
+	}
+
+	private boolean isRight(Node node) {
+		return node == node.parent.right;
+	}
+
+	private boolean isRoot(Node node) {
+		return node == root;
+	}
+
+	private boolean isValid(Node node) {
+		if (node == null)
+			return true;
+		if (node.left != null) {
+			if (node.left.val > node.val)
+				return false;
+			if (!isRoot(node) && isRight(node) && node.left.val < node.parent.val)
+				return false;
+		}
+		if (node.right != null) {
+			if (node.right.val < node.val)
+				return false;
+			if (!isRoot(node) && isLeft(node) && node.right.val > node.parent.val)
+				return false;
+		}
+		if (isValid(node.left) && isValid(node.right))
+			return true;
+		return false;
+	}
+
+	Node insert(int val) {
+		if (root == null) {
+			root = new Node();
+			root.val = val;
+			root.ht = 0;
+			root.left = root.right = null;
+			return root;
+		}
+		Node n = insert(null, root, val);
+		if (root.val > val) {
+			int h = height(root.left);
+			if (root.right == null)
+				root.ht = h + 1;
+			else
+				root.ht = 1 + (h > root.right.ht ? h : root.right.ht);
+		} else {
+			int h = height(root.right);
+			if (root.left == null)
+				root.ht = 1 + h;
+			else
+				root.ht = 1 + (h > root.left.ht ? h : root.left.ht);
+		}
+		System.out.println("n: " + n.val + " " + banlanceFactor(n));
+		if (n == null)
+			return root;
+
+		Node parent = findParent(root, n);
+		while (isNormal(n)) {
+			n = parent;
+			if (n == null)
+				return root;
+			parent = findParent(root, n);
+		}
+		if (parent != null)
+			System.out.println("parent: " + parent.val);
+		root = makeBalance(root, parent, n);
+
+		int leftH = height(root.left);
+		int rightH = height(root.right);
+		root.ht = 1 + (leftH > rightH ? leftH : rightH);
+		return root;
+	}
+
+	Node findParent(Node root, Node n) {
+		if (root == n)
+			return null;
+		if (root.left == n || root.right == n)
+			return root;
+		if (root.val > n.val)
+			return findParent(root.left, n);
+		else
+			return findParent(root.right, n);
+	}
+
+	Node makeBalance(Node root, Node parent, Node n) {
+		if (banlanceFactor(n) == 2) {
+			switch (banlanceFactor(n.left)) {
+			case -1:
+				leftR(n, n.left);
+				// fall throught
+			case 1:
+				if (root == n) {
+					root = n.left;
+				}
+				rightR(parent, n);
+				break;
+			default:
+				throw new IllegalArgumentException();
+			}
+		} else if (banlanceFactor(n) == -2) {
+			switch (banlanceFactor(n.right)) {
+			case 1:
+				rightR(n, n.right);
+				// fall throught
+			case -1:
+				if (root == n) {
+					root = n.right;
+				}
+				leftR(parent, n);
+				break;
+			default:
+				throw new IllegalArgumentException();
+			}
+		}
+		return root;
+	}
+
+	void leftR(Node parent, Node child) {
+		Node n = child.right;
+		if (parent != null) {
+			if (child == parent.left)
+				parent.left = n;
+			else
+				parent.right = n;
+		}
+		child.right = n.left;
+		n.left = child;
+	}
+
+	void rightR(Node parent, Node child) {
+		Node n = child.left;
+		if (parent != null) {
+			if (child == parent.left)
+				parent.left = n;
+			else
+				parent.right = n;
+		}
+		child.left = n.right;
+		n.right = child;
+	}
+
+	int banlanceFactor(Node node) {
+		if (node.left != null && node.right != null)
+			return node.left.ht - node.right.ht;
+		else if (node.left == null && node.right == null)
+			return 0;
+		else if (node.left != null)
+			return node.left.ht + 1;
+		else
+			return -1 - node.right.ht;
+	}
+
+	boolean isNormal(Node node) {
+		int bf = banlanceFactor(node);
+		if (bf >= -1 && bf <= 1)
+			return true;
+		return false;
+	}
+
+	void checkBalance(Node root, boolean isLeft) {
+		if (isLeft)
+			checkBalance(root, root.left, isLeft);
+		else
+			checkBalance(root, root.right, isLeft);
+		// else
+		// return;
+	}
+
+	void checkBalance(Node parent, Node child, boolean isLeft) {
+		if (child == null)
+			return;
+		if (isNormal(child)) {
+			checkBalance(child, child.left, true);
+			checkBalance(child, child.right, false);
+		} else {
+			return;
+		}
+	}
+
+	Node insert(Node parent, Node child, int val) {
+		if (child.val > val) {
+			if (child.left == null) {
+				child.left = create(child, val);
+				return parent;
+			} else {
+				return insert(child, child.left, val);
+			}
+		} else {
+			if (child.right == null) {
+				child.right = create(child, val);
+				return parent;
+			} else {
+				return insert(child, child.right, val);
+			}
+		}
+	}
+
+	Node create(Node parent, int val) {
+		Node child = new Node();
+		child.val = val;
+		child.ht = 0;
+		child.left = child.right = null;
+		if (parent != null) {
+			if (parent.val > val)
+				parent.left = child;
+			else
+				parent.right = child;
+		}
+		return child;
+	}
+
+	int height(Node node) {
+		if (node == null)
+			return -1;
+		int leftH = height(node.left);
+		int rightH = height(node.right);
+		node.ht = 1 + (leftH > rightH ? leftH : rightH);
+		return node.ht;
+	}
+
+	private void makeTree(Node parent, int[] data, int index) {
+		int left = index * 2 + 1;
+		if (left < data.length && data[left] != 0) {
+			Node node = new Node(data[left]);
+			parent.left = node;
+			node.parent = parent;
+			makeTree(node, data, left);
+		}
+		int right = index * 2 + 2;
+		if (right < data.length && data[right] != 0) {
+			Node node = new Node(data[right]);
+			parent.right = node;
+			node.parent = parent;
+			makeTree(node, data, right);
+		}
+	}
+
+	private static class Node {
+		private int val;
+		private Node left;
+		private Node right;
+		private Node parent;
+		private int ht;
+
+		public Node() {
+
+		}
+
+		public Node(int data, Node left, Node right, Node parent) {
+			this.val = data;
+			this.left = left;
+			this.right = right;
+			this.parent = parent;
+		}
+
+		public Node(int data) {
+			this(data, null, null, null);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		try (Scanner sc = new Scanner(System.in)) {
+			int size = sc.nextInt();
+			int[] data = new int[size];
+			for (int i = 0; i < size; i++)
+				data[i] = sc.nextInt();
+			AVLTree bst = new AVLTree(data);
+			bst.insert(sc.nextInt());
+			bst.printTree();
+			//System.out.println(bst.isValid());
+			//System.out.println(bst.sum());
+		}
+	}
+}
--- src/com/alex/test/BinSearchTree.java
+++ src/com/alex/test/BinSearchTree.java
@@ -1,5 +1,7 @@
 package com.alex.test;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BinSearchTree {
@@ -10,17 +12,100 @@ public class BinSearchTree {
 		root = new Node(data[0]);
 		makeTree(root, data, 0);
 	}
-
-	public void printTree() {
-		inorder(root);
-		preorder(root);
+	
+	public BinSearchTree() {
+		root = null;
 	}
 
+	public void printTree() {
+		if (root == null) {
+			System.out.println("Null tree");
+			return;
+		}
+		//preorder(root);
+		printTree(root, height(root));
+	}
+
+	private void printTree(Node n, int h) {
+		if (n == null)
+			return;
+		Queue<Node> ns = new LinkedList<>();
+		final Node DUMMY = new Node(-1);
+		ns.offer(n);
+		
+		int nPerLayer = 1;
+		int maxPerLayer = (int)Math.pow(2, h-1);
+		int unit = String.valueOf(last()).length() + 2;
+		if ((unit & 1) != 0)
+			unit++;
+		
+		for (int j = h; j > 0; j--) {
+			for (int i = 0; i < nPerLayer; i++) {
+				printIndent(nPerLayer, maxPerLayer, unit);
+				Node node = ns.poll();
+				if (node == DUMMY) {
+					printDummy(unit);
+					ns.offer(DUMMY);
+					ns.offer(DUMMY);
+				} else {
+					printVal(node.val, unit);
+					if (node.left == null)
+						ns.offer(DUMMY);
+					else
+						ns.offer(node.left);
+					if (node.right == null)
+						ns.offer(DUMMY);
+					else
+						ns.offer(node.right);
+				}
+				printIndent(nPerLayer, maxPerLayer, unit);
+			}
+			System.out.println();
+			nPerLayer *= 2;
+		}
+	}
+	
+	private void printIndent(int n, int max, int unit) {
+		for (int i = 0; i < (max*unit/n-unit)/2; i++)
+			System.out.print(" ");
+	}
+	
+	private void printVal(int val, int unit) {
+		int len = String.valueOf(val).length();
+		int pre, post;
+		pre = post = (unit - len) / 2;
+		if ((len & 1) != 0) {
+			pre++;
+		}
+		for (int i = 0; i < pre; i++)
+			System.out.print(" ");
+		System.out.print(val);
+		for (int i = 0; i < post; i++)
+			System.out.print(" ");
+	}
+	
+	private void printDummy(int unit) {
+		for (int i = 0; i < unit; i++)
+			System.out.print(" ");
+	}
+	
+	private int height(Node n) {
+		if (n == null)
+			return 0;
+		if (n.parent == null)
+			n.layer = 1;
+		else
+			n.layer = n.parent.layer + 1;
+		int left = height(n.left);
+		int right = height(n.right);
+		return 1 + (left > right ? left : right);
+	}
+	
 	private void inorder(Node node) {
 		if (node == null)
 			return;
 		inorder(node.left);
-		System.out.println(node.val + ", " + node.ht);
+		System.out.println(node.val);
 		inorder(node.right);
 	}
 	
@@ -34,6 +119,26 @@ public class BinSearchTree {
 
 	public boolean isValid() {
 		return isValid(root);
+	}
+	
+	private boolean isValid(Node node) {
+		if (node == null)
+			return true;
+		if (node.left != null) {
+			if (node.left.val > node.val)
+				return false;
+			if (!isRoot(node) && isRight(node) && node.left.val < node.parent.val)
+				return false;
+		}
+		if (node.right != null) {
+			if (node.right.val < node.val)
+				return false;
+			if (!isRoot(node) && isLeft(node) && node.right.val > node.parent.val)
+				return false;
+		}
+		if (isValid(node.left) && isValid(node.right))
+			return true;
+		return false;
 	}
 
 	public int sum() {
@@ -58,214 +163,180 @@ public class BinSearchTree {
 		return node == root;
 	}
 
-	private boolean isValid(Node node) {
-		if (node == null)
-			return true;
-		if (node.left != null) {
-			if (node.left.val > node.val)
-				return false;
-			if (!isRoot(node) && isRight(node) && node.left.val < node.parent.val)
-				return false;
-		}
-		if (node.right != null) {
-			if (node.right.val < node.val)
-				return false;
-			if (!isRoot(node) && isLeft(node) && node.right.val > node.parent.val)
-				return false;
-		}
-		if (isValid(node.left) && isValid(node.right))
-			return true;
-		return false;
+	public void insert(int val) {
+		insert(new Node(val));
 	}
-
-	Node insert(int val) {
-		if (root == null) {
-			root = new Node();
-			root.val = val;
-			root.ht = 0;
-			root.left = root.right = null;
-			return root;
-		}
-		Node n = insert(null, root, val);
-		if (root.val > val) {
-			int h = height(root.left);
-			if (root.right == null)
-				root.ht = h + 1;
+	
+	private void insert(Node n) {
+		Node p = null;
+		Node x = root;
+		while (x != null) {
+			p = x;
+			if (x.val > n.val)
+				x = x.left;
 			else
-				root.ht = 1 + (h > root.right.ht ? h : root.right.ht);
+				x = x.right;
+		}
+		n.parent = p;
+		if (p == null) {
+			root = n;
 		} else {
-			int h = height(root.right);
-			if (root.left == null)
-				root.ht = 1 + h;
+			if (p.val > n.val)
+				p.left = n;
 			else
-				root.ht = 1 + (h > root.left.ht ? h : root.left.ht);
+				p.right = n;
 		}
-		System.out.println("n: " + n.val + " " + banlanceFactor(n));
+	}
+	
+	public boolean delete(int val) {
+		Node n = find(val);
 		if (n == null)
-			return root;
-
-		Node parent = findParent(root, n);
-		while (isNormal(n)) {
-			n = parent;
-			if (n == null)
-				return root;
-			parent = findParent(root, n);
-		}
-		if (parent != null)
-			System.out.println("parent: " + parent.val);
-		root = makeBalance(root, parent, n);
-
-		int leftH = height(root.left);
-		int rightH = height(root.right);
-		root.ht = 1 + (leftH > rightH ? leftH : rightH);
-		return root;
+			return false;
+		delete(n);
+		return true;
 	}
-
-	Node findParent(Node root, Node n) {
-		if (root == n)
-			return null;
-		if (root.left == n || root.right == n)
-			return root;
-		if (root.val > n.val)
-			return findParent(root.left, n);
-		else
-			return findParent(root.right, n);
-	}
-
-	Node makeBalance(Node root, Node parent, Node n) {
-		if (banlanceFactor(n) == 2) {
-			switch (banlanceFactor(n.left)) {
-			case -1:
-				leftR(n, n.left);
-				// fall throught
-			case 1:
-				if (root == n) {
-					root = n.left;
-				}
-				rightR(parent, n);
-				break;
-			default:
-				throw new IllegalArgumentException();
-			}
-		} else if (banlanceFactor(n) == -2) {
-			switch (banlanceFactor(n.right)) {
-			case 1:
-				rightR(n, n.right);
-				// fall throught
-			case -1:
-				if (root == n) {
-					root = n.right;
-				}
-				leftR(parent, n);
-				break;
-			default:
-				throw new IllegalArgumentException();
-			}
-		}
-		return root;
-	}
-
-	void leftR(Node parent, Node child) {
-		Node n = child.right;
-		if (parent != null) {
-			if (child == parent.left)
-				parent.left = n;
-			else
-				parent.right = n;
-		}
-		child.right = n.left;
-		n.left = child;
-	}
-
-	void rightR(Node parent, Node child) {
-		Node n = child.left;
-		if (parent != null) {
-			if (child == parent.left)
-				parent.left = n;
-			else
-				parent.right = n;
-		}
-		child.left = n.right;
-		n.right = child;
-	}
-
-	int banlanceFactor(Node node) {
-		if (node.left != null && node.right != null)
-			return node.left.ht - node.right.ht;
-		else if (node.left == null && node.right == null)
-			return 0;
-		else if (node.left != null)
-			return node.left.ht + 1;
-		else
-			return -1 - node.right.ht;
-	}
-
-	boolean isNormal(Node node) {
-		int bf = banlanceFactor(node);
-		if (bf >= -1 && bf <= 1)
-			return true;
-		return false;
-	}
-
-	void checkBalance(Node root, boolean isLeft) {
-		if (isLeft)
-			checkBalance(root, root.left, isLeft);
-		else
-			checkBalance(root, root.right, isLeft);
-		// else
-		// return;
-	}
-
-	void checkBalance(Node parent, Node child, boolean isLeft) {
-		if (child == null)
-			return;
-		if (isNormal(child)) {
-			checkBalance(child, child.left, true);
-			checkBalance(child, child.right, false);
+	
+	private void delete(Node n) {
+		Node x;
+		if (n.left == null || n.right == null) {
+			x = n;
 		} else {
-			return;
+			x = higher(n);
 		}
-	}
-
-	Node insert(Node parent, Node child, int val) {
-		if (child.val > val) {
-			if (child.left == null) {
-				child.left = create(child, val);
-				return parent;
-			} else {
-				return insert(child, child.left, val);
-			}
+		Node child = (x.left == null) ? x.right : x.left;
+		if (child != null) {
+			child.parent = x.parent;
+		}
+		if (x.parent == null) {
+			root = child;
 		} else {
-			if (child.right == null) {
-				child.right = create(child, val);
-				return parent;
-			} else {
-				return insert(child, child.right, val);
-			}
-		}
-	}
-
-	Node create(Node parent, int val) {
-		Node child = new Node();
-		child.val = val;
-		child.ht = 0;
-		child.left = child.right = null;
-		if (parent != null) {
-			if (parent.val > val)
-				parent.left = child;
+			if (x == x.parent.left)
+				x.parent.left = child;
 			else
-				parent.right = child;
+				x.parent.right = child;
 		}
-		return child;
+		
+		if (x != n) {
+			n.val = x.val;
+		}
+		x.parent = null;
+		x.left = null;
+		x.right = null;
 	}
-
-	int height(Node node) {
-		if (node == null)
-			return -1;
-		int leftH = height(node.left);
-		int rightH = height(node.right);
-		node.ht = 1 + (leftH > rightH ? leftH : rightH);
-		return node.ht;
+	
+	private Node find(int val) {
+		Node x = root;
+		while (x != null) {
+			if (x.val == val)
+				return x;
+			if (x.val > val)
+				x = x.left;
+			else
+				x = x.right;
+		}
+		return x;
+	}
+	
+	public int first() {
+		return first(root).val;
+	}
+	
+	private Node first(Node x) {
+		Node p = x;
+		while (x != null) {
+			p = x;
+			x = x.left;
+		}
+		return p;
+	}
+	
+	public int last() {
+		return last(root).val;
+	}
+	
+	private Node last(Node x) {
+		Node p = x;
+		while (x != null) {
+			p = x;
+			x = x.right;
+		}
+		return p;
+	}
+	
+	public int higher(int val) {
+		Node p = null;
+		Node x = root;
+		while (x != null) {
+			if (x.val == val)
+				break;
+			p = x;
+			if (x.val > val)
+				x = x.left;
+			else
+				x = x.right;
+		}
+		if (x == null && p == null)
+			return Node.INVALID;
+		else if (x == null) {
+			if (p.val > val)
+				return p.val;
+			else
+				x = p;
+		}
+		Node n = higher(x);
+		if (n == null)
+			return Node.INVALID;
+		else
+			return n.val;
+	}
+	
+	private Node higher(Node n) {
+		if (n.right != null)
+			return first(n.right);
+		Node p = n.parent;
+		while (p != null && p.right == n) {
+			n = p;
+			p = n.parent;
+		}
+		return p;
+	}
+	
+	public int lower(int val) {
+		Node p = null;
+		Node x = root;
+		while (x != null) {
+			if (x.val == val)
+				break;
+			p = x;
+			if (x.val > val)
+				x = x.left;
+			else
+				x = x.right;
+		}
+		if (x == null && p == null)
+			return Node.INVALID;
+		else if (x == null) {
+			if (p.val < val)
+				return p.val;
+			else
+				x = p;
+		}
+		Node n = lower(x);
+		if (n == null)
+			return Node.INVALID;
+		else
+			return n.val;
+	}
+	
+	private Node lower(Node n) {
+		if (n.left != null)
+			return last(n.left);
+		Node p = n.parent;
+		while (p != null && p.left == n) {
+			n = p;
+			p = n.parent;
+		}
+		return p;
 	}
 
 	private void makeTree(Node parent, int[] data, int index) {
@@ -290,21 +361,19 @@ public class BinSearchTree {
 		private Node left;
 		private Node right;
 		private Node parent;
-		private int ht;
-
-		public Node() {
-
-		}
-
-		public Node(int data, Node left, Node right, Node parent) {
-			this.val = data;
+		int layer;
+		private static final int INVALID = -1;
+		
+		public Node(int val, Node left, Node right, Node parent) {
+			this.val = val;
 			this.left = left;
 			this.right = right;
 			this.parent = parent;
+			this.layer = 0;
 		}
 
-		public Node(int data) {
-			this(data, null, null, null);
+		public Node(int val) {
+			this(val, null, null, null);
 		}
 	}
 
@@ -312,14 +381,26 @@ public class BinSearchTree {
 
 		try (Scanner sc = new Scanner(System.in)) {
 			int size = sc.nextInt();
-			int[] data = new int[size];
-			for (int i = 0; i < size; i++)
-				data[i] = sc.nextInt();
-			BinSearchTree bst = new BinSearchTree(data);
-			bst.insert(sc.nextInt());
+			BinSearchTree bst = new BinSearchTree();
+			for (int i = 0; i < size; i++) {
+				bst.insert(sc.nextInt());
+				bst.printTree();
+				System.out.println();
+			}
+			/*bst.delete(5);
+			bst.printTree();*/
+			bst.delete(4);
 			bst.printTree();
-			//System.out.println(bst.isValid());
-			//System.out.println(bst.sum());
+			
+			System.out.println(bst.first());
+			System.out.println(bst.last());
+			System.out.println(bst.lower(6));
+			System.out.println(bst.lower(9));
+			System.out.println(bst.higher(9));
+			System.out.println(bst.higher(4));
+			bst.printTree();
+
+
 		}
 	}
 }
