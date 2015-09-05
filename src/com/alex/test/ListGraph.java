3fb1de1aa4ece7390421a56f09142e910dc7949d
zcao0709 <zcao0709@gmail.com>
2015-09-05 17:07:43 +0800

    dijkstra

--- src/com/alex/test/ListGraph.java
+++ src/com/alex/test/ListGraph.java
@@ -1,6 +1,8 @@
 package com.alex.test;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -17,23 +19,24 @@ public class ListGraph {
 		}
 	}
 	
-	public void addEdge(int s, int d) {
+	public void addEdge(int s, int d, int w) {
 		Vertex start = vs[s - 1];
-		Edge dest = new Edge(vs[d - 1]);
+		Edge dest = new Edge(vs[d - 1], w);
 		dest.next = start.edges;
 		start.edges = dest;
 		
 		start = vs[d - 1];
-		dest = new Edge(vs[s - 1]);
+		dest = new Edge(vs[s - 1], w);
 		dest.next = start.edges;
 		start.edges = dest;
 	}
 	
 	public void setStart(int i) {
 		start = i - 1;
-		vs[start].path = 0;
+		vs[start].dist = 0;
 	}
 	
+	// only for fixed weight
 	public void bfs() {
 		Queue<Vertex> q = new LinkedList<>();
 		vs[start].color = Color.BLACK;
@@ -47,11 +50,36 @@ public class ListGraph {
 				Vertex v = e.dest;
 				if (v.color == Color.WHILTE) {
 					v.color = Color.BLACK;
-					v.path = start.path + e.weight;
+					v.dist = start.dist + e.weight;
 					q.offer(v);
 				} else {
-					if (v.path > start.path + e.weight)
-						v.path = start.path + e.weight;
+					if (v.dist > start.dist + e.weight)
+						v.dist = start.dist + e.weight;
+				}
+				e = e.next;
+			}
+		}
+	}
+	
+	public void dijkstra() {
+		Queue<Vertex> q = new PriorityQueue<>(vs.length, new Comparator<Vertex>() {
+			@Override
+			public int compare(Vertex v1, Vertex v2) {
+				return v2.dist - v1.dist;
+			}
+		});
+		q.offer(vs[start]);
+		while (q.size() > 0) {
+			Vertex start = q.poll();
+			Edge e = start.edges;
+			while (e != null) {
+				//System.out.println(start);
+				//System.out.println(e);
+				Vertex v = e.dest;
+				if (v.dist == -1 || v.dist > start.dist + e.weight) {
+					v.dist = start.dist + e.weight;
+					v.pred = start;
+					q.offer(v);
 				}
 				e = e.next;
 			}
@@ -62,7 +90,7 @@ public class ListGraph {
 		for (int i = 0; i < vs.length; i++) {
 			if (i == start)
 				continue;
-			System.out.print(vs[i].path + " ");
+			System.out.print(vs[i].dist + " ");
 		}
 		System.out.println();
 	}
@@ -84,13 +112,15 @@ public class ListGraph {
 	private static class Vertex {
 		int val;
 		Color color;
-		int path;
+		int dist;
+		Vertex pred;
 		Edge edges;
 		
 		Vertex(int v) {
 			val = v;
 			color = Color.WHILTE;
-			path = -1;
+			dist = -1;
+			pred = null;
 			edges = null;
 		}
 		@Override
@@ -105,13 +135,17 @@ public class ListGraph {
 		Edge next;
 		
 		Edge(Vertex d) {
+			this(d, WEIGHT);
+		}
+		
+		Edge(Vertex d, int w) {
 			dest = d;
-			weight = WEIGHT;
+			weight = w;
 			next = null;
 		}
 		@Override
 		public String toString() {
-			return "->" + dest;
+			return "->" + dest + weight;
 		}
 	}
 	
@@ -124,9 +158,9 @@ public class ListGraph {
 			int e = sc.nextInt();
 			ListGraph g = new ListGraph(v);
 			for (int j = 0; j < e; j++)
-				g.addEdge(sc.nextInt(), sc.nextInt());
+				g.addEdge(sc.nextInt(), sc.nextInt(), sc.nextInt());
 			g.setStart(sc.nextInt());
-			g.bfs();
+			g.dijkstra();
 			g.printPath();
 		}
 		sc.close();
