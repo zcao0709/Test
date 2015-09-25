3a61b8916c31892b5ffef3c82fc5b0fd3efd5d75
zcao0709 <zcao0709@gmail.com>
2015-09-25 09:43:49 +0800

    ListGraph - dfs

--- src/com/alex/test/ListGraph.java
+++ src/com/alex/test/ListGraph.java
@@ -2,15 +2,21 @@ package com.alex.test;
 
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.List;
 import java.util.Scanner;
 
 public class ListGraph {
 
-	private static int WEIGHT = 6;
 	private Vertex[] vs;
 	private int start;
+	
+	private ListGraph(Vertex[] vs) {
+		this.vs = new Vertex[vs.length];
+		for (int i = 0; i < vs.length; i++) {
+			this.vs[i] = new Vertex(vs[i]);
+		}
+	}
 	
 	public ListGraph(int nNode) {
 		vs = new Vertex[nNode];
@@ -20,24 +26,62 @@ public class ListGraph {
 	}
 	
 	public void addEdge(int s, int d, int w) {
-		Vertex start = vs[s - 1];
-		Edge dest = new Edge(vs[d - 1], w);
+		addEdge(s, d, w, false);
+	}
+	
+	public void addEdge(int s, int d, int w, boolean undirected) {
+		
+		Vertex start = vs[s];
+		Edge dest = new Edge(vs[d], w);
 		dest.next = start.edges;
 		start.edges = dest;
 		
-		start = vs[d - 1];
-		dest = new Edge(vs[s - 1], w);
-		dest.next = start.edges;
-		start.edges = dest;
+		if (undirected) {
+			start = vs[d];
+			dest = new Edge(vs[s], w);
+			dest.next = start.edges;
+			start.edges = dest;
+		}
 	}
 	
 	public void setStart(int i) {
-		start = i - 1;
+		start = i;
 		vs[start].dist = 0;
 	}
 	
-	// only for fixed weight
+	public int getStart() {
+		return start;
+	}
+	
+	public ListGraph transpose() {
+		ListGraph g = new ListGraph(vs);
+		for (Vertex v : g.vs) {
+			v.edges = null;
+		}
+		for (Vertex v : vs) {
+			Edge e = v.edges;
+			while (e != null) {
+				g.addEdge(e.dest.idx, v.idx, e.weight, false);
+				e = e.next;
+			}
+		}
+		g.setStart(start);
+		//System.out.println(vs[0] + "," + g.vs[0]);
+		return g;
+	}
+	
+	private void init() {
+		for (Vertex v : vs) {
+			v.color = Color.WHILTE;
+			v.pred = null;
+			v.dist = v == vs[start] ? 0 : -1;
+			//v.dt = 0;
+			//v.ft = 0;
+		}
+	}
+	
 	public void bfs() {
+		init();
 		Queue<Vertex> q = new LinkedList<>();
 		vs[start].color = Color.GREY;
 		q.offer(vs[start]);
@@ -50,6 +94,7 @@ public class ListGraph {
 				Vertex v = e.dest;
 				if (v.color == Color.WHILTE) {
 					v.color = Color.GREY;
+					v.dist = start.dist + 1;
 					v.pred = start;
 					q.offer(v);
 				}
@@ -57,6 +102,51 @@ public class ListGraph {
 			}
 			start.color = Color.BLACK;
 		}
+	}
+	
+	public void dfs(int start) {
+		init();
+		
+		int time = dfsVisit(vs[start], 0);
+		for (Vertex v : vs) {
+			if (v.color == Color.WHILTE)
+				time = dfsVisit(v, time);
+		}
+	}
+	
+	public void dfs() {
+		int time = 0;
+		PHeap<Vertex> heap = new PHeap<>(vs.length, new Comparator<Vertex>() {
+			@Override
+			public int compare(Vertex v1, Vertex v2) {
+				return v2.ft - v1.ft;
+			}
+		});
+		for (Vertex v : vs)
+			heap.offer(v);
+		while (heap.size() > 0) {
+			Vertex v = heap.poll();
+			if (v.color == Color.WHILTE)
+				time = dfsVisit(v, time);
+		}
+	}
+	
+	private int dfsVisit(Vertex v, int time) {
+		v.color = Color.GREY;
+		time++;
+		v.dt = time;
+		Edge e = v.edges;
+		while (e != null) {
+			Vertex u = e.dest;
+			if (u.color == Color.WHILTE) {
+				u.pred = v;
+				time = dfsVisit(u, time);
+			}
+			e = e.next;
+		}
+		v.color = Color.BLACK;
+		v.ft = ++time;
+		return time;
 	}
 	
 	public void dijkstra() {
@@ -102,6 +192,7 @@ public class ListGraph {
 			printPath(v);
 			System.out.println();
 		}
+		System.out.println();
 	}
 	
 	private void printPath(Vertex v) {
@@ -117,6 +208,23 @@ public class ListGraph {
 		}
 	}
 	
+	public void printTree() {
+		for (Vertex v : vs) {
+			if (v.ft - v.dt == 1) {
+				printTree(v);
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
+	private void printTree(Vertex v) {
+		if (v.pred != null) {
+			printTree(v.pred);
+		}
+		System.out.print(v + " ");
+	}
+	
 	public void printDist() {
 		for (int i = 0; i < vs.length; i++) {
 			if (i == start)
@@ -124,6 +232,23 @@ public class ListGraph {
 			System.out.print(vs[i].dist + " ");
 		}
 		System.out.println();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Vertex v : vs) {
+			sb.append(v);
+			if (v == vs[start])
+				sb.append("*");
+			Edge e = v.edges;
+			while (e != null) {
+				sb.append(e).append(" ");
+				e = e.next;
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 	
 	private static enum Color {
@@ -142,23 +267,38 @@ public class ListGraph {
 	
 	private static class Vertex {
 		int val;
+		int idx;
 		Color color;
 		int dist;
+		int dt; //discovery time in dfs
+		int ft; //finish time in dfs
 		Vertex pred;
 		Edge edges;
 		
-		Vertex(int v) {
+		Vertex(int v, int i) {
 			val = v;
+			idx = i;
 			color = Color.WHILTE;
 			dist = -1;
 			pred = null;
 			edges = null;
 		}
 		
+		Vertex(int v) {
+			this(v, v - 1);
+		}
+		
+		Vertex(Vertex v) {
+			this(v.val, v.idx);
+			dt = v.dt;
+			ft = v.ft;
+		}
+		
 		@Override
 		public String toString() {
 			//return val + color.toString() + dist;
-			return String.valueOf(val);
+			return val + "-" + dt + "-" + ft;
+			//return String.valueOf(val);
 		}
 	}
 	
@@ -168,7 +308,7 @@ public class ListGraph {
 		Edge next;
 		
 		Edge(Vertex d) {
-			this(d, WEIGHT);
+			this(d, 1);
 		}
 		
 		Edge(Vertex d, int w) {
@@ -176,12 +316,31 @@ public class ListGraph {
 			weight = w;
 			next = null;
 		}
+		
 		@Override
 		public String toString() {
-			return "->" + dest + weight;
+			return "->" + dest;
 		}
 	}
-	
+	/*
+1
+8 14
+1 2
+2 3
+2 5
+2 6
+3 4
+3 7
+4 3
+4 8
+5 1
+5 6
+6 7
+7 6
+7 8
+8 8
+3
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -191,10 +350,15 @@ public class ListGraph {
 			int e = sc.nextInt();
 			ListGraph g = new ListGraph(v);
 			for (int j = 0; j < e; j++)
-				g.addEdge(sc.nextInt(), sc.nextInt(), sc.nextInt());
-			g.setStart(sc.nextInt());
-			g.bfs();
-			g.printPath();
+				g.addEdge(sc.nextInt()-1, sc.nextInt()-1, 1);
+			g.setStart(sc.nextInt()-1);
+			System.out.println(g);
+			g.dfs(g.getStart());
+			g.printTree();
+			ListGraph tg = g.transpose();
+			tg.dfs();
+			tg.printTree();
+			System.out.println(tg);
 		}
 		sc.close();
 	}
